552b9d6bbdafe6aa3aecd56fc1462c18170d7764
Satapitchakorn Punjarat <satapitchakorn.punjarat@allianz.com>
2020-08-05 12:27:41 +0700

    Step 02.

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,13 +12,30 @@ import java.util.List;
 
 @RestController
 public class UserController {
+    private int item_per_page = 10;
+    private int page = 1;
 
     @GetMapping("/users")
-    public ResponseEntity<?> getAllUsers() {
+    public ResponseEntity<?> getAllUsers(@RequestParam(required = false) String page, @RequestParam(value = "item_per_page", required = false) String ipp) {
+
+        if (page != null) {
+            this.page = Integer.parseInt(page);
+        }
+        if (ipp != null) {
+            this.item_per_page = Integer.parseInt(ipp);
+        }
         List<UsersResponse> users = new ArrayList() {{
             add(new UsersResponse(1, "User 1"));
             add(new UsersResponse(2, "User 2"));
         }};
-        return new ResponseEntity<>(users, HttpStatus.OK);
+        UserPage up = new UserPage();
+        up.setPage(page == null ? this.page : Integer.parseInt(page));
+        up.setItem_per_page(ipp == null ? this.item_per_page : Integer.parseInt(ipp));
+        return new ResponseEntity<>(up, HttpStatus.OK);
+    }
+
+    @GetMapping("/users/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable int id) {
+        return new ResponseEntity<>(new UsersResponse(id, "User " + id), HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/demo/UserPage.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import lombok.Data;
+
+@Data
+public class UserPage {
+    private int page;
+
+    private int item_per_page;
+}
