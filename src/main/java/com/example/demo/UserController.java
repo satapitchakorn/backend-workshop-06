38f2017f9b74b38b5adc35c2299f88001c114376
Satapitchakorn Punjarat <satapitchakorn.punjarat@allianz.com>
2020-08-05 13:26:10 +0700

    Edit.

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -12,25 +12,16 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    private int item_per_page = 10;
-    private int page = 1;
-
     @GetMapping("/users")
-    public ResponseEntity<?> getAllUsers(@RequestParam(required = false) String page, @RequestParam(value = "item_per_page", required = false) String ipp) {
+    public ResponseEntity<?> getAllUsers(@RequestParam(required = false, defaultValue = "1") String page, @RequestParam(value = "item_per_page", required = false, defaultValue = "10") String ipp) {
 
-        if (page != null) {
-            this.page = Integer.parseInt(page);
-        }
-        if (ipp != null) {
-            this.item_per_page = Integer.parseInt(ipp);
-        }
         List<UsersResponse> users = new ArrayList() {{
             add(new UsersResponse(1, "User 1"));
             add(new UsersResponse(2, "User 2"));
         }};
         UserPage up = new UserPage();
-        up.setPage(page == null ? this.page : Integer.parseInt(page));
-        up.setItem_per_page(ipp == null ? this.item_per_page : Integer.parseInt(ipp));
+        up.setPage(Integer.parseInt(page));
+        up.setItem_per_page(Integer.parseInt(ipp));
         return new ResponseEntity<>(up, HttpStatus.OK);
     }
 
